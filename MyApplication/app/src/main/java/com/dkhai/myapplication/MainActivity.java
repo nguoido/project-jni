b2987b604bb52fee18ade333371a81b7ed1af3c6
Do Khai <dovankhai@gmail.com>
2020-10-27 17:10:17 +0700

    change cpp to jni
        /*
        externalNativeBuild {
            cmake {
                path "src/main/cpp/CMakeLists.txt"
                version "3.10.2"
            }
        }
        */
    
        externalNativeBuild {
            ndkBuild {
                path "src/main/jni/Android.mk"
            }
        }

--- MyApplication/app/src/main/java/com/dkhai/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/dkhai/myapplication/MainActivity.java
@@ -25,11 +25,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        TextView  tv = new TextView(this);
+        tv.setText( stringFromJNI() );
+        setContentView(tv);
     }
 
     /**
--- MyApplication/app/src/main/jni/native-lib.c
+++ MyApplication/app/src/main/jni/native-lib.c
@@ -7,5 +7,5 @@
 
 jstring Java_com_dkhai_myapplication_MainActivity_stringFromJNI( JNIEnv* env, jobject thiz )
 {
-    return (*env)->NewStringUTF(env, "Hello from C");
+    return (*env)->NewStringUTF(env, "Xin chao");
 }
